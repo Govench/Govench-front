b99ffb3f53f9f53b5794402bfdfa42e4270ee977
Gino Guevara <gino_josue_13@hotmail.com>
2024-09-18 01:53:57 -0500

    feat: Continuando con el Landing-page

--- Govench/src/app/app.routes.ts
+++ Govench/src/app/app.routes.ts
@@ -1,4 +1,13 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
 
+/* @NgModule({
+
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{
+    
+}  */

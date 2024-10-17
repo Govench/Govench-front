8b1c136a6532dfb16c95795016e618108dd059c7
Miguel Angel Escobar Gomez <maeg2468@gmail.com>
2024-10-17 00:55:06 -0500

    creando componente

--- Govench/src/app/app.routes.ts
+++ Govench/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ManageEventsComponent } from './manage-events/manage-events.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'events/crear', component: ManageEventsComponent},
+
+];
 
 /* @NgModule({
 

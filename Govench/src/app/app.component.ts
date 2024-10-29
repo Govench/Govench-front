5e0086279f354b22148430afc088214f5a2718b4
Dylan Acevedo Villena <dylan.acevedo2843@gmail.com>
2024-10-29 06:18:27 -0500

    feat:Implementacion estática de login

--- Govench/src/app/app.component.ts
+++ Govench/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavComponent } from './nav/nav.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,CarruselComponent,BodyComponent,FooterComponent],
+  imports: [RouterOutlet, NavComponent, CarruselComponent, BodyComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

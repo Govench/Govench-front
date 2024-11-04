1b57dd66c3160f3d5b40c604354ef7a0f61c7758
jaime <jpalominoc5@upao.edu.pe>
2024-11-03 22:26:54 -0500

    feat: adding recovering password

--- Govench/src/app/app.routes.ts
+++ Govench/src/app/app.routes.ts
@@ -2,10 +2,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { BodyComponent } from './body/body.component';
+import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 
 export const routes: Routes = [
    
     { path: 'auth', loadChildren : () => import ("././pages/auth/auth.routes").then(a => a.authRoutes)},
+    { path: 'password-recovery', component: PasswordRecoveryComponent },
     { path: '**', redirectTo: 'auth/login', pathMatch: 'full' }
+    
 ];
 

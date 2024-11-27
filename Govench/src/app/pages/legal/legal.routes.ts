a27ad2f7338df00b479c4b6d8c23ce64f13ddc71
Ginnn13 <gino_josue_13@hotmail.com>
2024-11-26 19:59:55 -0500

    feat: Ver comunidades a las que perteneces

--- Govench/src/app/pages/legal/legal.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';  
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
-import { PrivacyPoliticsComponent } from './privacy-politics/privacy-politics.component';
-
-export const legalRoutes: Routes = [
-    { path : 'privacy', component: PrivacyPoliticsComponent },
-    { path : 'terms', component: TermsConditionsComponent }
-];
